d666144cfb564cd5dca87bf25e5e355ad1ee6d6f
Malte Krupa <github-nqyroppg@nafn.de>
2022-05-14 07:24:56 +0200

    Replace miners multiple_select with a text_input
    multiple_select assumes values are already present for a run and you
    just choose which ones you'd like to select.
    
    Having a text_input that splits words into the array we need in the db
    is hacky for multiple reasons:
    1. Users need to know how to input values in this field
    2. Inputs need to be altered both when entered and when retrieved
    
    But at the same time it is an easy way to alter this data without
    touching any frontend code.

--- lib/scdb_web/live/run_live/form_component.ex
+++ lib/scdb_web/live/run_live/form_component.ex
@@ -23,7 +23,19 @@ defmodule ScdbWeb.RunLive.FormComponent do
     {:noreply, assign(socket, :changeset, changeset)}
   end
 
+  @impl true
+  def handle_event("miners_changed", %{"run" => run_params}, socket) do
+    run_params = make_miners_an_array(run_params)
+
+    changeset =
+      socket.assigns.run
+      |> Mining.change_run(run_params)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
   def handle_event("save", %{"run" => run_params}, socket) do
+    run_params = make_miners_an_array(run_params)
     save_run(socket, socket.assigns.action, run_params)
   end
 
@@ -51,5 +63,12 @@ defmodule ScdbWeb.RunLive.FormComponent do
       {:error, %Ecto.Changeset{} = changeset} ->
         {:noreply, assign(socket, changeset: changeset)}
     end
+  end
+
+  defp make_miners_an_array(run_params) do
+    array =
+      run_params["miners"]
+      |> String.split([",", " ", "\n"], trim: true)
+    %{run_params | "miners" => array}
   end
 end
